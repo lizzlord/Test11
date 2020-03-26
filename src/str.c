3261563a70aac80bdcbbc287d39b4543093c5ebf
lordinit <61364097+lordinit@users.noreply.github.com>
2020-03-26 23:21:05 +0300

    test

---
+++ src/str.c
@@ -0,0 +1,19 @@
+///LALA
+void ft_putchar(char c);
+
+void		ft_putstr(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+void main (void)
+{
+	char str[] = "helloworld";
+	ft_putstr(str);
+}
